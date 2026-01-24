f6ffb93ac2a1deceab4f3fca519775650839db26
Ramz <ramzahmedm@gmail.com>
2026-01-24 05:07:34 +0000

    Decided to restart backend and frontend for feed due to intended changes to database for future additions

--- Backend/models.py
+++ Backend/models.py
@@ -12,36 +12,3 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
-# Relationship for posts
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    content = Column(Text)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-    author = relationship("User")
-
-
-
-#Relationship for Comments
-class Comment(Base):
-    __tablename__ = "comments"
-
-    id = Column(Integer, primary_key=True, index=True)
-    content = Column(Text)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    post_id = Column(Integer, ForeignKey("posts.id"))
-
-#Relationship for Likes
-class Like(Base):
-    __tablename__ = "likes"
-
-    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-    post_id = Column(Integer, ForeignKey("posts.id"), primary_key=True)
-
-    #ensure a user can like a post only once
-    __table_args__ = (
-        UniqueConstraint('user_id', 'post_id', name='_user_post_uc'),
-        )
--- Backend/schemas.py
+++ Backend/schemas.py
@@ -2,50 +2,13 @@ from pydantic import BaseModel
 from sqlalchemy.sql.sqltypes import DateTime
 from datetime import datetime
 
-#Schemas for User creation and login
+#Schemas for user creation
 class UserCreate(BaseModel):
     email: str
     password: str
 
+#Schemas for user login
 class UserLogin(BaseModel):
     email: str
     password: str
 
-#Schemas for Post creation and output
-class PostCreate(BaseModel):
-    content: str
-
-class PostOut(BaseModel):
-    id: int
-    content: str
-    user_id: int
-
-    #Easy conversion from ORM objects for sqlalchemy
-    class Config:
-       from_attributes =  True
-
-#Schemas for comment creation
-class CommentCreate(BaseModel):
-    content: str
-
-class CommentOut(BaseModel):
-    id: int
-    content: str
-    user_id: int
-    post_id: int
-
-    #Easy conversion from ORM objects for sqlalchemy
-    class Config:
-        from_attributes = True
-
-
-#Schema for Post feed output with author email and timestamp
-class PostFeedOut(BaseModel):
-    id: int
-    content: str
-    author_email: str
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
-        
